25b924d9e566590110efb4c923a32e43f0b4790d
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-09-24 02:13:15 +0300

    add rtl & ltr

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <div
-    class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
-  >
-    <SiteNavigation/>
-    <HomeView/>
+  <div dir="rtl" v-if=" lang === 'ar' "
+    class="flex flex-col min-h-screen font-Roboto bg-weather-primary ">
+    <SiteNavigation />
+    <HomeView />
+  </div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary " v-if=" lang === 'en' ">
+    <SiteNavigation />
+    <HomeView />
   </div>
 </template>
 
 <script setup>
-import SiteNavigation from "./components/SiteNavigation.vue";
+
+import SiteNavigation from "./components/SiteNavigation.vue"
 import HomeView from "./views/HomeView.vue"
+
+const lang = localStorage.getItem( "lang" )
 </script>
 
 <style>
