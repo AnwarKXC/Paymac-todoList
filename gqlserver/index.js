4f1cc220d0d8885081caa196d26b27a4099409b8
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-09-26 22:19:11 +0300

    edit cors server

--- gqlserver/index.js
+++ gqlserver/index.js
@@ -1,45 +1,65 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
 import { typeDefs } from './schema.js'
 import db from './_db.js'
+import express from 'express'
 import { uid } from 'uid'
+import cors from 'cors'
+import http from 'http'
+import bodyParser from 'body-parser'
+import { expressMiddleware } from '@apollo/server/express4'
+
+
+
 const resolvers = {
    Query: {
-      todos: () =>db.todo,
-      todo: ( _, args ) =>db.todo.find( ( todo ) => todo.id === args.id )
+      todos: () => db.todo,
+      todo: ( _, args ) => db.todo.find( ( todo ) => todo.id === args.id )
    },
    Mutation: {
-      deleteTodo ( _ , args ) {
+      deleteTodo ( _, args ) {
          db.todo = db.todo.filter( todo => todo.id !== args.id )
          return db.todo
       },
-      addTodo (_,args) {
+      addTodo ( _, args ) {
          let todo = {
             ...args.todo,
-            id:uid(),
+            id: uid(),
          }
-         db.todo.push(todo)
+         db.todo.push( todo )
          return todo
       },
       updateTodo ( _, args ) {
          db.todo = db.todo.map( ( todo ) => {
             if ( todo.id === args.id ) {
-               return { ...todo , ...args.edits}
+               return { ...todo, ...args.edits }
             }
             return todo
          } )
-         return db.todo.find((todo)=>todo.id===args.id)
+         return db.todo.find( ( todo ) => todo.id === args.id )
       }
    }
 }
 // server setup
+
+const app = express()
+const httpServer = http.createServer( app )
+
+
 const server = new ApolloServer( {
    typeDefs,
    resolvers
 } )
 
-const { url } = await startStandaloneServer( server, {
-   listen: { port: 4000 }
-} )
+await server.start()
 
-console.log( `🚀  Server ready at: ${ url }` )
+app.use(
+   '/graphql',
+   cors( {
+      origin: [ 'http://localhost:3000', 'http://localhost:3001'] } ),
+   bodyParser.json( ),
+   expressMiddleware( server )
+)
+
+await new Promise( ( resolve ) => httpServer.listen( { port: 8000 }, resolve ) )
+
+console.log( `🚀 Server ready at http://localhost:8000/graphql` )
--- src/main.js
+++ src/main.js
@@ -23,17 +23,14 @@ app.mount( "#app" )
 
 // // HTTP connection to the API
 // const httpLink = createHttpLink( {
-//    // You should use an absolute URL here
-//    uri: 'http://localhost:4000',
+//    uri: 'http://localhost:8000/graphql',
 // } )
 
-// // Cache implementation
-// const cache = new InMemoryCache()
 
 // // Create the apollo client
 // const apolloClient = new ApolloClient( {
 //    link: httpLink,
-//    cache,
+//    cache: new InMemoryCache(),
 // } )
 
 // const app = createApp( {
